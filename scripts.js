e258b1c97df7e61029cb4e3432db11a13b3c4968
Adelino Almeida <adelinoalmeida101@gmail.com>
2023-10-31 14:01:39 -0300

    Add files via upload

--- scripts.js
+++ scripts.js
@@ -1,11 +1,13 @@
 document.addEventListener('DOMContentLoaded', function() {
     const grid = document.getElementById('grid');
     const resetBtn = document.getElementById('reset');
-    const traps = ["Armadilha de urso: dano +1", "Sonífero: não joga (1x)",
-    "Tranquilizante: ataque e esquiva pela metade", "Empalamento: dano +3", "Praga: reduz tudo em -2 (2x)",
-    "Hipnose: ataque amigo (1x)", "Sange-suga: dano +1", "Arpão: ataque +3 (1x)",
-    "Soco glês de prata: ataque +1", "Semente dos deuses: +2 de vida", "Semente dos deuses: +2 de vida",
-    "Semente dos deuses: +2 de da", "Semente dos deuses: +2 de vida", "Semente dos deuses: +2 de vida"];
+    const appearBtn = document.getElementById('appear'); 
+
+    const traps = ["Armadilha de urso: dano +1", "Sonífero: parado (1x)", "Arpão: ataque +3 (1x)",
+    "Tranquilizante: ataque e esquiva -2", "Empalamento: dano +3", "Praga: reduz tudo em -2 (2x)",
+    "Hipnose: ataque amigo (1x)", "Sange-suga: dano +1", "Soco glês de prata: ataque +1",
+    "Semente dos deuses: +2 de vida", "Semente dos deuses: +2 de vida", "Semente dos deuses: +2 de vida",
+    "Semente dos deuses: +2 de vida", "Semente dos deuses: +2 de vida"];
 
     function createButton(id) {
         const button = document.createElement('button');
@@ -36,7 +38,29 @@ document.addEventListener('DOMContentLoaded', function() {
         }
     }
 
+    function appearRandomButton() {
+        const randomButtonNumber = Math.floor(Math.random() * 49) + 1; 
+        const selectedButton = document.querySelector(`[data-id="${randomButtonNumber}"]`);
+        
+        if (selectedButton.classList.contains('blue')) {
+            selectedButton.classList.remove('blue');
+            selectedButton.classList.add('green');
+        } else if (selectedButton.classList.contains('green')) {
+            selectedButton.classList.remove('green');
+            selectedButton.classList.add('blue');
+        } else if (selectedButton.classList.contains('red')) {
+            selectedButton.classList.remove('red');
+            selectedButton.classList.add('purple');
+        } else if (selectedButton.classList.contains('purple')) {
+            selectedButton.classList.remove('purple');
+            selectedButton.classList.add('red');
+        } else {
+            selectedButton.classList.add('blue'); 
+        }
+    }
+
     resetBtn.addEventListener('click', resetGrid);
+    appearBtn.addEventListener('click', appearRandomButton); 
 
     resetGrid();
 });
